80a517a89483c3f167d1542831ea54d3856d4341
Aarushi Bisht <aarushiibisht@gmail.com>
2020-04-09 17:18:02 -0400

    added templates and javascript for storage unit UI

--- airavata_mft/settings.py
+++ airavata_mft/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'airavata_mft.apps.workspace'
+    'airavata_mft.apps.workspace.apps.WorkspaceConfig'
 ]
 
 MIDDLEWARE = [
