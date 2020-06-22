83d32fdcb94cf0d06fb5049af711ee132b2246f6
Akhil Nagulavancha <akhil@akhils-mbp.lan>
2020-06-22 16:47:53 -0400

    Improved readability

--- airavata_mft/apps/workspace/static/airavata_mft_workspace/js/containers/StorageResources.vue
+++ airavata_mft/apps/workspace/static/airavata_mft_workspace/js/containers/StorageResources.vue
@@ -1,48 +1,55 @@
 <template>
-    <div class="row">
-        <div class="col-7">
-            <h1 class="h4 mb-4">{{heading}}</h1>
-        </div>
-        <div class="col-4">
-            <div class="btn-toolbar">
-                <div class="btn-group mr-4" role="group">
-                    <div class="dropdown">
-                        <button class="btn btn-sm btn-outline-secondary dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Sort by</button>
-                        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                            <a class="dropdown-item" href="#" @click="sortBy('name')">Name</a>
-                            <a class="dropdown-item" href="#" @click="sortBy('size')">Size</a>
-                        </div>
-                    </div>
-                </div>
-                <div class="btn-group mr-4" role="group">
-                    <button class="btn btn-sm btn-outline-secondary list"><i class="fa fa-bars"></i></button>
-                    <button class="btn btn-sm btn-outline-secondary list"><i class="fa fa-th"></i></button>
-                </div>
-            </div>
-        </div>
-        <table class="table table-hover main-table">
-                <thead>
-                    <tr>
-                      <th scope="col">Name</th>
-                      <th scope="col">Size</th>
-                      <th scope="col">Last Modified</th>
-                      <th scope="col"></th>
-                    </tr>
-                </thead>
-                  <tbody>
-                    <tr v-for="resource in resources"
+	<div class="row">
+		<div class="col-7">
+			<h1 class="h4 mb-4">{{heading}}</h1>
+		</div>
+		<div class="col-4">
+			<div class="btn-toolbar">
+				<div class="btn-group mr-4" role="group">
+					<div class="dropdown">
+						<button class="btn btn-sm btn-outline-secondary dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Sort by</button>
+						<div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+							<a class="dropdown-item" href="#" @click="sortBy('name')">Name</a>
+							<a class="dropdown-item" href="#" @click="sortBy('size')">Size</a>
+						</div>
+					</div>
+				</div>
+				<div class="btn-group mr-4" role="group">
+					<button class="btn btn-sm btn-outline-secondary list">
+						<i class="fa fa-bars"></i>
+					</button>
+					<button class="btn btn-sm btn-outline-secondary list">
+						<i class="fa fa-th"></i>
+					</button>
+				</div>
+			</div>
+		</div>
+		<table class="table table-hover main-table">
+			<thead>
+				<tr>
+					<th scope="col">Name</th>
+					<th scope="col">Size</th>
+					<th scope="col">Last Modified</th>
+					<th scope="col"></th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr v-for="resource in resources"
                         :key="resource.resourceId">
-                      <th scope="row"><i class="fa fa-folder resource-name-icon"></i>
-{{resource.name}}</th>
-                      <td>{{resource.size}}</td>
-                      <td>{{resource.lastModified}}</td>
-                      <td><b-button class="btn btn-info btn-sm transfer">Transfer</b-button></td>
-                    </tr>
-                  </tbody>
-                </table>
-        </div>
+					<th scope="row">
+						<i class="fa fa-folder resource-name-icon"></i>
+{{resource.name}}
+					</th>
+					<td>{{resource.size}}</td>
+					<td>{{resource.lastModified}}</td>
+					<td>
+						<b-button class="btn btn-info btn-sm transfer">Transfer</b-button>
+					</td>
+				</tr>
+			</tbody>
+		</table>
+	</div>
 </template>
-
 <script>
     export default {
         name: "StorageResources.vue",
@@ -59,8 +66,8 @@
         }
     }
     }
-</script>
 
+</script>
 <style scoped>
     .main-table{
         width: 83%!important;
