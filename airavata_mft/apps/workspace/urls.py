f6973cf5cb4a04990559efc08e40a60c4f6eee4a
Akhil Nagulavancha <akhil@Akhils-MacBook-Pro.local>
2020-07-22 05:41:38 -0400

    added move to homescreen

--- airavata_mft/apps/workspace/urls.py
+++ airavata_mft/apps/workspace/urls.py
@@ -1,7 +1,8 @@
 from . import views
 from django.conf.urls import url
 
+app_name='workspace'
 urlpatterns = [
-    url(r'^storage/$', views.storage),
-    url(r'^storage/(?P<storage_id>[^/]+)/$', views.resources)
+    url(r'^storage/$', views.storage,name="storages"),
+    url(r'^storage/(?P<storage_id>[^/]+)/$', views.resources,name="resources")
 ]
